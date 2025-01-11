4711cb7d9082ca4cbcf5c5becb95364179cf8051
Szymon Rogowski <rogowski@student.agh.edu.pl>
2025-01-11 02:48:49 +0100

    notes and comments

--- System_Configs/netplan/netplan.sh
+++ System_Configs/netplan/netplan.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Netplan is a utility developed by Canonical, the company behind Ubuntu. 
+# It provides a network configuration abstraction over the currently supported two “backend” system (“renderer” in Netplan terminology): 
+# networkd and NetworkManager.
+
 sudo vim /etc/netplan/01-network-manager-all.yaml
 
 sudo netplan apply
@@ -13,7 +17,7 @@ grep -n "net.ipv4.ip_forward" /etc/sysctl.conf
 echo 1 > /proc/sys/net/ipv4/ip_forward
 cat /proc/sys/net/ipv4/ip_forward
 
-
+# Enforce changes
 sysctl -p
 
 
@@ -21,6 +25,7 @@ sysctl -p
 ping 172.20.136.2
 ping 10.0.1.2
 
+# Check if data is being gathered
 sudo tcpdump -i ens18 > ens18.txt
 sudo tcpdump -i ens19 > ens19.txt
 sudo tcpdump -i ens20 > ens20.txt
